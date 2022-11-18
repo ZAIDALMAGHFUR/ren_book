641de8a086457b27c4ed7fcc21fd3804baceb6ad
Zaid al maghfur <91768583+ZAIDALMAGHFUR@users.noreply.github.com>
2022-11-18 07:44:00 +0700

    clear make public pages

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
     public function index()
     {
-        return view('book-list');
+        $books = Book::all();
+        return view('book-list', ['books' => $books]);
     }
 }
--- resources/views/book-list.blade.php
+++ resources/views/book-list.blade.php
@@ -2,5 +2,17 @@
 @section('title', 'Book')
 
 @section('content')
-    
+<div class="row my-5">
+    @foreach ($books as $b)
+        <div class="card" style="width: 18rem; margin-left: 10px; margin-bottom: 10px">
+            <img src="{{ ('images/storage/cover/'.$b->cover) }}" class="card-img-top" alt="..." style="border-radius: 10px; margin: 10px;" draggable="false">
+                <div class="card-body">
+                <h5 class="card-title">{{ $b->book_code}}</h5>
+                <p class="card-text">{{ $b->title }}</p>
+                <a href="#" class="btn {{ $b->status == 'in stock' ? 'btn-info' : 'btn-danger' }} ">{{ $b->status }}</a>
+            </div>
+        </div>
+    @endforeach
+</div>
+
 @endsection
